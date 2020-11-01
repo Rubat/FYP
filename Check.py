885c0ccc18e6ad83b96a50186679cbe6c98356b6
Rubat Majeed <rubata=majeed1@gmail.com>
2020-11-01 22:42:26 +0500

    1

--- Check.py
+++ Check.py
@@ -77,9 +77,9 @@ if a in leftheight :
 lw = statistics.mode(leftwidth)
 lh = statistics.mode(leftheight)
 
-if b in rightwidth :
+if b in rightwidth:
     rightwidth.remove(b)
-if a in rightwidth :
+if a in rightwidth:
     rightheight.remove(a)
 
 rw = statistics.mode(rightwidth)
@@ -217,34 +217,50 @@ while cap.isOpened():
             # hsv1 = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2RGB)
             hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_RGB2HSV)
 
+            # define range of red color in HSV
             lower_red = np.array([110, 50, 50])
             upper_red = np.array([130, 255, 255])
-            mask2 = cv2.inRange(hsv, lower_red, upper_red)
+            # define range of orange color in HSV
+            lower_orange = np.array([91, 50, 50])
+            upper_orange = np.array([111, 255, 255])
+            # define range of brown color in HSV
+            lower_brown = np.array([102, 50, 50])
+            upper_brown = np.array([122, 255, 255])
+            # define range of green color in HSV
+            lower_green = np.array([50, 100, 100])
+            upper_green = np.array([70, 255, 255])
+            # define range of blue color in HSV
+            lower_blue = np.array([154, 2, 21])
+            upper_blue = np.array([174, 255, 255])
 
-            mask3 = np.zeros(frame.shape, dtype=np.uint8)
-            cv2.fillPoly(mask3, pts=[c], color=(255, 255, 255))
-            masked_image = cv2.bitwise_and(frame, mask3)
+            # Threshold the HSV image to get only blue colors
+            mask_red = cv2.inRange(hsv, lower_red, upper_red)
+            mask_orange = cv2.inRange(hsv, lower_orange, upper_orange)
+            mask_brown = cv2.inRange(hsv, lower_brown, upper_brown)
+            mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+            mask_green = cv2.inRange(hsv, lower_green, upper_green)
 
-            contours, _ = cv2.findContours(mask2, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+            frcontours, _ = cv2.findContours(mask_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+            focontours, _ = cv2.findContours(mask_orange, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+            fbcontours, _ = cv2.findContours(mask_brown, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+            fblcontours, _ = cv2.findContours(mask_blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+            fgcontours, _ = cv2.findContours(mask_green, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-            for contour in contours:
-                cv2.drawContours(frame1, contour, -1, (0, 0, 0), 1)
-
-            for b in contours:
-                if cv2.contourArea(b) < 250:
-                    continue
-                # get the bounding rect
-                elif cv2.contourArea(b) > 350:
-                    x, y, w, h = cv2.boundingRect(b)
-                    # print(x,y,w,h)
-                    # draw a green rectangle to visualize the bounding rect2
-                    cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 1)
-                    mask3 = np.zeros(frame.shape, dtype=np.uint8)
-                    cv2.fillPoly(mask3, pts=[c], color=(255, 255, 255))
-
-                    # apply the mask
-                    masked_image = cv2.bitwise_and(roi, mask3)
-
+            # for fc in frcontours:
+            #     if cv2.contourArea(fc) > 250:
+            #         cv2.drawContours(roi, fc, -1, (0, 0, 0), 2)
+            for fc in focontours:
+                if cv2.contourArea(fc) > 250:
+                    cv2.drawContours(roi, fc, -1, (0, 0, 0), 2)
+            # for fc in fbcontours:
+            #     if cv2.contourArea(fc) > 250:
+            #         cv2.drawContours(roi, fc, -1, (0, 0, 0), 2)
+            # for fc in fblcontours:
+            #     if cv2.contourArea(fc) > 250:
+            #         cv2.drawContours(roi, fc, -1, (0, 0, 0), 2)
+            # for fc in fgcontours:
+            #     if cv2.contourArea(fc) > 250:
+            #         cv2.drawContours(roi, fc, -1, (0, 0, 0), 2)
 
     # cv2.imshow('result', masked_image)
     if contFound == False:
@@ -252,7 +268,7 @@ while cap.isOpened():
         cv2.rectangle(frame, (pRx, pRy), (pRx + pRw, pRy + pRh), (255, 255, 255), 2)
 
 
-    cv2.imshow('result', frame1)
+    cv2.imshow('result', frame)
     # cv2.imshow('result', frame1)
     # cv2.imshow('result', roi2)
     k = cv2.waitKey(15) & 0xFF
