5f9fab9d440c05be37171437065935617ef10d7c
Rubat-Majeed <75979714+Rubat-Majeed@users.noreply.github.com>
2021-01-29 17:28:31 +0500

    Latest Code

--- Check.py
+++ Check.py
@@ -297,6 +297,64 @@ while cap.isOpened():
         cv2.rectangle(frame, (pLx, pLy), (pLx + pLw, pLy + pLh), (255, 255, 255), 2)
         cv2.rectangle(frame, (pRx, pRy), (pRx + pRw, pRy + pRh), (255, 255, 255), 2)
 
+        upper_left = (x, y)
+        bottom_right = (x + w, y + h)
+        roi = frame[upper_left[1]: bottom_right[1], upper_left[0]: bottom_right[0]]
+
+        cw = w / 2
+        ch = h / 2
+        cv2.circle(roi, (int(cw), int(ch)), 3, (0, 0, 0), -1)
+
+        blurred_frame = cv2.GaussianBlur(roi, (5, 5), 0)
+
+        hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_RGB2HSV)
+
+        # define range of red color in HSV
+        lower_red = np.array([110, 50, 50])
+        upper_red = np.array([130, 255, 255])
+        # define range of orange color in HSV
+        lower_orange = np.array([91, 50, 50])
+        upper_orange = np.array([111, 255, 255])
+        # define range of brown color in HSV
+        lower_brown = np.array([102, 50, 50])
+        upper_brown = np.array([122, 255, 255])
+        # define range of green color in HSV
+        lower_green = np.array([25, 52, 72])
+        upper_green = np.array([102, 255, 255])
+        # define range of blue color in HSV
+        lower_blue = np.array([154, 2, 21])
+        upper_blue = np.array([174, 255, 255])
+
+        # Threshold the HSV image to get only blue colors
+        mask_red = cv2.inRange(hsv, lower_red, upper_red)
+        mask_orange = cv2.inRange(hsv, lower_orange, upper_orange)
+        mask_brown = cv2.inRange(hsv, lower_brown, upper_brown)
+        mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+        mask_green = cv2.inRange(hsv, lower_green, upper_green)
+
+        frcontours, _ = cv2.findContours(mask_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        focontours, _ = cv2.findContours(mask_orange, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        fbcontours, _ = cv2.findContours(mask_brown, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        fblcontours, _ = cv2.findContours(mask_blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        fgcontours, _ = cv2.findContours(mask_green, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+        # code for the dots for all the finger colors
+        Dots(frcontours, roi, x, y, 'red')
+        for i in range(0, len(pXRDot)):
+            cv2.circle(frame, (int(pXRDot[i]), int(pYRDot[i])), 3, (0, 0, 255, i), -1)
+        Dots(focontours, roi, x, y, 'orange')
+        for i in range(0, len(pXODot)):
+            cv2.circle(frame, (int(pXODot[i]), int(pYODot[i])), 3, (0, 255, 255, 0), -1)
+        Dots(fbcontours, roi, x, y, 'brown')
+        for i in range(0, len(pXBDot)):
+            cv2.circle(frame, (int(pXBDot[i]), int(pYBDot[i])), 3, (0, 50, 255, 0), -1)
+        Dots(fblcontours, roi, x, y, 'blue')
+        for i in range(0, len(pXBLDot)):
+            cv2.circle(frame, (int(pXBLDot[i]), int(pYBLDot[i])), 3, (255, 0, 0, 0), -1)
+        Dots(fgcontours, roi, x, y, 'green')
+        for i in range(0, len(pXGDot)):
+            cv2.circle(frame, (int(pXGDot[i]), int(pYGDot[i])), 3, (0, 255, 0, 0), -1)
+
 
     cv2.imshow('result', frame)
   #  cv2.imshow('mask red' , mask_red)
