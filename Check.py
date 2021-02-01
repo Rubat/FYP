d5a4f5f4439ae28903e1fedc6b01c75383851460
Talha Asjad <talha.asjad.ta@gmail.com>
2021-02-02 01:11:57 +0500

    Csv File is being generated. Before running the code everytime you need to delete the csv file because if you don't do it it will start appending on the old values so delete old csv file before running. This code will generate a new csv file automatically.

--- Check.py
+++ Check.py
@@ -3,23 +3,69 @@ import numpy as np
 import sys
 import statistics
 import time
+import csv
+from csv import DictWriter
+from csv import writer
 
+pXRDot = []
+pYRDot = []
 
-def Dots(fcontours,roi):
+pXODot = []
+pYODot = []
 
+pXBDot = []
+pYBDot = []
+
+pXBLDot = []
+pYBLDot = []
+
+pXGDot = []
+pYGDot = []
+def Dots(fcontours,roi, x, y, color):
+    fx=0
+    fy=0
     for fc in fcontours:
-            if cv2.contourArea(fc) > 400:                   
-                (fx,fy,fw,fh) = cv2.boundingRect(fc)
-                upper_left = (fx, fy)
-                bottom_right = (fx + fw, fy + fh)
-                roi1 = roi[upper_left[1]: bottom_right[1], upper_left[0]: bottom_right[0]]
+        if cv2.contourArea(fc) > 400:                   
+            (fx,fy,fw,fh) = cv2.boundingRect(fc)
+            upper_left = (fx, fy)
+            bottom_right = (fx + fw, fy + fh)
+            roi1 = roi[upper_left[1]: bottom_right[1], upper_left[0]: bottom_right[0]]
 
-                fw = fw/2
-                fh = fh/2
-                cv2.circle(roi1, (int(fw), int(fh)), 3, (255, 255, 255), -1)
+            fw = fw/2
+            fh = fh/2
+            if color == 'red':
+                pXRDot.append(fx+x)
+                pYRDot.append(fy+y)
+            if color == 'orange':
+                pXODot.append(fx+x)
+                pYODot.append(fy+y)
+            if color == 'brown':
+                pXBDot.append(fx+x)
+                pYBDot.append(fy+y)
+            if color == 'blue':
+                pXBLDot.append(fx+x)
+                pYBLDot.append(fy+y)
+            if color == 'green':
+                pXGDot.append(fx+x)
+                pYGDot.append(fy+y)
+            cv2.circle(roi1, (int(fw), int(fh)), 3, (255, 255, 255), -1)
+    #return fx, fy
+
+    
+
+    #Function to append in CSV 
+def append_dict_as_row(file_name, dict_of_elem, field_names):
+    # Open file in append mode
+    with open(file_name, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        dict_writer = DictWriter(write_obj, fieldnames=field_names)
+        # Add dictionary as wor in the csv
+        dict_writer.writerow(dict_of_elem)
+
 
 # Reading Video
-cap = cv2.VideoCapture('new1.mp4')
+vidname = 'new1.mp4'
+cap = cv2.VideoCapture(vidname)
 
 # Background Subtraction
 mask = cv2.createBackgroundSubtractorMOG2(history=1, varThreshold=15, detectShadows=False)
@@ -63,7 +109,7 @@ while cap.isOpened():
         if cv2.contourArea(c) < 500:
             continue
         elif cv2.contourArea(c) > 1000:
-           
+
             (x, y, w, h) = cv2.boundingRect(c)
 
             if x < b / 2:
@@ -102,7 +148,8 @@ print(int(avgw),int(avgh))
 
 
 # Reading Video
-cap = cv2.VideoCapture('new1.mp4')
+
+cap = cv2.VideoCapture(vidname)
 
 # Background Subtraction
 mask = cv2.createBackgroundSubtractorMOG2(history=1, varThreshold=15, detectShadows=False)
@@ -121,12 +168,12 @@ pLw=0
 pRw=0
 pLh=0
 pRh=0
-p1 = 0.5
-p2 = 0.5
+p1 = 0.3
+p2 = 0.7
 
 
 while cap.isOpened():
-    time.sleep(0)
+    # time.sleep(0.25)
     ret, frame = cap.read()
     if not ret:
         break
@@ -223,14 +270,11 @@ while cap.isOpened():
             lower_brown = np.array([102, 50, 50])
             upper_brown = np.array([122, 255, 255])
             # define range of green color in HSV
-            lower_green = np.array([25, 52, 72])
+            lower_green = np.array([25,52,72])
             upper_green = np.array([102, 255, 255])
             # define range of blue color in HSV
-            lower_blue = np.array([94, 90, 2])
-            upper_blue = np.array([126, 255, 255])
-            # define range of red color in HSV
-            low_purple = np.array([120, 100, 100])
-            high_purple = np.array([165, 255, 255])
+            lower_blue = np.array([154, 2, 21])
+            upper_blue = np.array([174, 255, 255])
 
             # Threshold the HSV image to get only blue colors
             mask_red = cv2.inRange(hsv, lower_red, upper_red)
@@ -238,7 +282,6 @@ while cap.isOpened():
             mask_brown = cv2.inRange(hsv, lower_brown, upper_brown)
             mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
             mask_green = cv2.inRange(hsv, lower_green, upper_green)
-            purple_mask = cv2.inRange(hsv, low_purple, high_purple)
 
             frcontours, _ = cv2.findContours(mask_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
             focontours, _ = cv2.findContours(mask_orange, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
@@ -247,31 +290,124 @@ while cap.isOpened():
             fgcontours, _ = cv2.findContours(mask_green, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
             # code for the dots for all the finger colors
-            Dots(frcontours,roi)
-            Dots(focontours,roi)
-            Dots(fbcontours,roi)
-            Dots(fblcontours,roi)
-            Dots(fgcontours,roi)
+            Dots(frcontours,roi, x ,y, 'red')
+            for i in range (0, len(pXRDot)):
+                cv2.circle(frame, (int(pXRDot[i]), int(pYRDot[i])), 3, (0, 0, 255, i), -1)
+            Dots(focontours,roi, x,y, 'orange')
+            for i in range (0, len(pXODot)):
+               cv2.circle(frame, (int(pXODot[i]), int(pYODot[i])), 3, (0, 255, 255, 0), -1)
+            Dots(fbcontours,roi , x,y, 'brown' )
+            for i in range (0, len(pXBDot)):
+               cv2.circle(frame, (int(pXBDot[i]), int(pYBDot[i])), 3, (0, 50, 255, 0), -1)
+            Dots(fblcontours,roi, x,y, 'blue')
+            for i in range (0, len(pXBLDot) ):  
+               cv2.circle(frame, (int(pXBLDot[i]), int(pYBLDot[i])), 3, (255, 0, 0, 0), -1)
+            Dots(fgcontours,roi, x,y, 'green')
+            for i in range (0, len(pXGDot)):
+               cv2.circle(frame, (int(pXGDot[i]), int(pYGDot[i])), 3, (0, 255, 0, 0), -1)
 
     #cv2.imshow('result', masked_image)
     if contFound == False:
         cv2.rectangle(frame, (pLx, pLy), (pLx + pLw, pLy + pLh), (255, 255, 255), 2)
         cv2.rectangle(frame, (pRx, pRy), (pRx + pRw, pRy + pRh), (255, 255, 255), 2)
 
+        upper_left = (x, y)
+        bottom_right = (x + w, y + h)
+        roi = frame[upper_left[1]: bottom_right[1], upper_left[0]: bottom_right[0]]
+
+        cw = w / 2
+        ch = h / 2
+        cv2.circle(roi, (int(cw), int(ch)), 3, (0, 0, 0), -1)
+
+        blurred_frame = cv2.GaussianBlur(roi, (5, 5), 0)
+
+        hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_RGB2HSV)
+
+        # define range of red color in HSV
+        lower_red = np.array([110, 50, 50])
+        upper_red = np.array([130, 255, 255])
+        # define range of orange color in HSV
+        lower_orange = np.array([91, 50, 50])
+        upper_orange = np.array([111, 255, 255])
+        # define range of brown color in HSV
+        lower_brown = np.array([102, 50, 50])
+        upper_brown = np.array([122, 255, 255])
+        # define range of green color in HSV
+        lower_green = np.array([25, 52, 72])
+        upper_green = np.array([102, 255, 255])
+        # define range of blue color in HSV
+        lower_blue = np.array([154, 2, 21])
+        upper_blue = np.array([174, 255, 255])
+
+        # Threshold the HSV image to get only blue colors
+        mask_red = cv2.inRange(hsv, lower_red, upper_red)
+        mask_orange = cv2.inRange(hsv, lower_orange, upper_orange)
+        mask_brown = cv2.inRange(hsv, lower_brown, upper_brown)
+        mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+        mask_green = cv2.inRange(hsv, lower_green, upper_green)
+
+        frcontours, _ = cv2.findContours(mask_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        focontours, _ = cv2.findContours(mask_orange, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        fbcontours, _ = cv2.findContours(mask_brown, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        fblcontours, _ = cv2.findContours(mask_blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        fgcontours, _ = cv2.findContours(mask_green, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+        
+        # code for the dots for all the finger colors and saving their coordinates in csv file
+
+        
+        #Setting fields for CSV file
+        fields =['RedX','RedY','OrangeX','OrangeY', 'BrownX','BrownY','BlueX','BlueY','GreenX','GreenY' ]
+        filename= "Coordinates of hands.csv"
+
+        Dots(frcontours, roi, x, y, 'red')
+        for i in range(0, len(pXRDot)):    
+            mydict ={'RedX': pXRDot[i],'RedY': pYRDot[i]}
+            append_dict_as_row(filename, mydict, fields)
+            cv2.circle(frame, (int(pXRDot[i]), int(pYRDot[i])), 3, (0, 0, 255, i), -1)
+            
+
+        Dots(focontours, roi, x, y, 'orange')
+        for i in range(0, len(pXODot)):
+            mydict={'OrangeX':pXODot[i],'OrangeY':pYODot[i]}
+            append_dict_as_row(filename, mydict, fields)
+            cv2.circle(frame, (int(pXODot[i]), int(pYODot[i])), 3, (0, 255, 255, 0), -1)
+
+        Dots(fbcontours, roi, x, y, 'brown')
+        for i in range(0, len(pXBDot)):
+            mydict={'BrownX':pXBDot[i],'BrownY':pYBDot[i]}
+            append_dict_as_row(filename, mydict, fields)
+            cv2.circle(frame, (int(pXBDot[i]), int(pYBDot[i])), 3, (0, 50, 255, 0), -1)
+
+        Dots(fblcontours, roi, x, y, 'blue')
+        for i in range(0, len(pXBLDot)):
+            mydict={'BlueX':pXBLDot[i],'BlueY':pYBLDot[i]}
+            append_dict_as_row(filename, mydict, fields)
+            cv2.circle(frame, (int(pXBLDot[i]), int(pYBLDot[i])), 3, (255, 0, 0, 0), -1)
+
+        Dots(fgcontours, roi, x, y, 'green')
+        for i in range(0, len(pXGDot)):
+            mydict={'GreenX':pXGDot[i],'GreenY':pYGDot[i]}
+            append_dict_as_row(filename, mydict, fields)
+            cv2.circle(frame, (int(pXGDot[i]), int(pYGDot[i])), 3, (0, 255, 0, 0), -1)
+
 
     cv2.imshow('result', frame)
+
   #  cv2.imshow('mask red' , mask_red)
   #  cv2.imshow('mask orange', mask_orange)
-  # cv2.imshow('mask2', mask)
+   #cv2.imshow('mask2', mask)
+
     cv2.imshow('result', frame1)
+ 
   #  cv2.imshow('result', roi2)
     k = cv2.waitKey(15) & 0xFF
     if k == 27:
         break
     countL = 0
     countR = 0
-    it += 1
-# This is where the video is read
-
+    it += 1 
+              
+      
 cap.release()
 cv2.destroyAllWindows()
